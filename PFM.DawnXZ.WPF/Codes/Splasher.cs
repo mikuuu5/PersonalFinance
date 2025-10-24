8f16789b875a13d18f325c425bb50cc64749263a
Jayramisty <2774640286@qq.com>
2025-10-24 11:45:46 +0800

    Update Splasher.cs

--- PFM.DawnXZ.WPF/Codes/Splasher.cs
+++ PFM.DawnXZ.WPF/Codes/Splasher.cs
@@ -1,34 +1,23 @@
-﻿using System;
+using System;
 using System.Windows;
-
 namespace PFM.DawnXZ.WPF.Codes
 {
-    /// <summary>
     /// Helper to show or close given splash window
-    /// </summary>
     public static class Splasher
     {
-        /// <summary>
         /// Get or set the splash screen window
-        /// </summary>
         public static Window Splash { get; set; }
-        /// <summary>
         /// Show splash screen
-        /// </summary>
         public static void ShowSplash()
         {
             if (Splash != null) Splash.Dispatcher.BeginInvoke(new Action(() => { Splash.Show(); }));
         }
-        /// <summary>
         /// Hide splash screen
-        /// </summary>
         public static void HideSplash()
         {
             if (Splash != null) Splash.Dispatcher.BeginInvoke(new Action(() => { Splash.Hide(); }));
         }
-        /// <summary>
         /// Close splash screen
-        /// </summary>
         public static void CloseSplash()
         {
             if (Splash != null)
