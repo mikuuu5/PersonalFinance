017be2030bb753bb786f1c7ecc25b5a38cb691d3
Jayramisty <2774640286@qq.com>
2025-10-24 09:44:15 +0800

    Update DictionaryTsit.cs

--- PFM.DawnXZ.Library/Transit/DictionaryTsit.cs
+++ PFM.DawnXZ.Library/Transit/DictionaryTsit.cs
@@ -1,29 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using PFM.DawnXZ.Library.Entity;
 using PFM.DawnXZ.Library.BLL;
-
 namespace PFM.DawnXZ.Library.Transit
 {
-    /// <summary>
-    /// 账务字典
-    /// </summary>
+    //账务字典
     public class DictionaryTsit
     {
-        /// <summary>
-        /// 获取所有数据
-        /// <remarks>
-        /// 排序：字典类型、字典标识
-        /// </remarks>
-        /// </summary>
+        // 获取所有数据
+        /// <remarks>排序：字典类型、字典标识</remarks>
         /// <returns></returns>
         public static IList<PfmDictionaryMDL> ISelect()
         {
             return PfmDictionaryBLL.ISelect(null, "dict_type,dict_mark");
         }
-        /// <summary>
-        /// 获得报表类型名称
-        /// </summary>
+        //获得报表类型名称
         /// <param name="dictMark">类型编号</param>
         /// <returns></returns>
         public static string GetDictName(byte dictMark)
@@ -48,55 +39,35 @@ namespace PFM.DawnXZ.Library.Transit
             }
             return dictName;
         }
-
         #region 键值对
-
         #region 数据状态
-        /// <summary>
-        /// 数据状态键值对
-        /// <remarks>
-        /// 1 正常 / 0 禁用，Key / Value
-        /// </remarks>
-        /// </summary>
+        //数据状态键值对
+        /// <remarks>1 正常 / 0 禁用，Key / Value</remarks>
         public static Dictionary<string, int> Status
         {
             get { return BuildStatus(); }
         }
-        /// <summary>
-        /// 数据状态键值对
-        /// <remarks>
-        /// 1 正常 / 0 禁用
-        /// </remarks>
-        /// </summary>
+        //数据状态键值对
+        /// <remarks>1 正常 / 0 禁用</remarks>
         /// <returns>Key / Value</returns>
         private static Dictionary<string, int> BuildStatus()
         {
             Dictionary<string, int> dict = new Dictionary<string, int>()
             {
-                {"正常",1}
-                ,{"禁用",0}
+                {"正常",1},{"禁用",0}
             };
             return dict;
         }
         #endregion 数据状态
-
         #region 请选择
-        /// <summary>
-        /// 下拉框默认项键值对[请选择]
-        /// <remarks>
-        /// -1 请选择
-        /// </remarks>
-        /// </summary>
+        //下拉框默认项键值对[请选择]
+        /// <remarks> -1 请选择</remarks>
         public static Dictionary<string, int> DefSelect
         {
             get { return BuildDefSelect(); }
         }
-        /// <summary>
-        /// 下拉框默认项键值对[请选择]
-        /// <remarks>
-        /// -1 请选择
-        /// </remarks>
-        /// </summary>
+        //下拉框默认项键值对[请选择]
+        /// <remarks>-1 请选择</remarks>
         /// <returns>Key / Value</returns>
         private static Dictionary<string, int> BuildDefSelect()
         {
@@ -107,24 +78,15 @@ namespace PFM.DawnXZ.Library.Transit
             return dict;
         }
         #endregion 请选择
-
         #region 顶级分类
-        /// <summary>
-        /// 下拉框默认项键值对[顶级分类]
-        /// <remarks>
-        /// -1 顶级分类
-        /// </remarks>
-        /// </summary>
+        //下拉框默认项键值对[顶级分类]
+        /// <remarks> -1 顶级分类</remarks>
         public static Dictionary<string, int> DefCategory
         {
             get { return BuildDefCategory(); }
         }
-        /// <summary>
-        /// 下拉框默认项键值对[顶级分类]
-        /// <remarks>
-        /// -1 顶级分类
-        /// </remarks>
-        /// </summary>
+        //下拉框默认项键值对[顶级分类]
+        /// <remarks>-1 顶级分类</remarks>
         /// <returns>Key / Value</returns>
         private static Dictionary<string, int> BuildDefCategory()
         {
@@ -135,67 +97,44 @@ namespace PFM.DawnXZ.Library.Transit
             return dict;
         }
         #endregion 顶级分类
-
         #region 收支项目类型
-        /// <summary>
-        /// 收支项目类型键值对
-        /// <remarks>
-        /// 1名称/2地址/3描述
-        /// </remarks>
-        /// </summary>
+        //收支项目类型键值对
+        /// <remarks>1名称/2地址/3描述</remarks>
         public static Dictionary<string, int> IaeItemsType
         {
             get { return BuildIaeItemsType(); }
         }
-        /// <summary>
-        /// 收支项目类型键值对
-        /// <remarks>
-        /// 1名称/2地址/3描述
-        /// </remarks>
-        /// </summary>
+        //收支项目类型键值对
+        /// <remarks>1名称/2地址/3描述</remarks>
         /// <returns>Key / Value</returns>
         private static Dictionary<string, int> BuildIaeItemsType()
         {
             Dictionary<string, int> dict = new Dictionary<string, int>()
             {
-                {"名称",1}
-                ,{"地址",2}
-                ,{"描述",3}
+                {"名称",1},{"地址",2},{"描述",3}
             };
             return dict;
         }
         #endregion 收支项目类型
-
         #region 收支明细正负值
-        /// <summary>
-        /// 收支明细正负值键值对
-        /// <remarks>
-        /// 1 正常 / 0 禁用，Key / Value
-        /// </remarks>
-        /// </summary>
+        // 收支明细正负值键值对
+        /// <remarks>1 正常 / 0 禁用，Key / Value</remarks>
         public static Dictionary<string, int> IncomingsOrOutgoings
         {
             get { return BuildIncomingsOrOutgoings(); }
         }
-        /// <summary>
-        /// 收支明细正负值键值对
-        /// <remarks>
-        /// 1 正常 / 0 禁用
-        /// </remarks>
-        /// </summary>
+        //收支明细正负值键值对
+        /// <remarks> 1 正常 / 0 禁用 </remarks>
         /// <returns>Key / Value</returns>
         private static Dictionary<string, int> BuildIncomingsOrOutgoings()
         {
             Dictionary<string, int> dict = new Dictionary<string, int>()
             {
-                {"支出",-1}
-                ,{"收入",1}
+                {"支出",-1} ,{"收入",1}
             };
             return dict;
         }
         #endregion 收支明细正负值
-
         #endregion 键值对
-
     }
 }
