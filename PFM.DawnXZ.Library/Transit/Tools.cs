010bba58669f2a34bed74ef649137e1e6753ba98
Jayramisty <2774640286@qq.com>
2025-10-24 10:54:37 +0800

    Update Tools.cs

--- PFM.DawnXZ.Library/Transit/Tools.cs
+++ PFM.DawnXZ.Library/Transit/Tools.cs
@@ -2,28 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 namespace PFM.DawnXZ.Library.Transit
 {
-    /// <summary>
-    /// 工具类
-    /// </summary>
+    //工具类
     public class Tools
     {
         #region Variable & 变量
-
         #endregion Variable & 变量
-
         #region Instance variables & 实例变量
-
         #endregion Instance variables & 实例变量
-
         #region Constant & 常量
-
         #endregion Constant & 常量
-
         #region Attribute & 属性
-
         #region Path & 路径
         /// <summary>
         /// 应用程序路径·WPF
@@ -68,10 +58,7 @@ namespace PFM.DawnXZ.Library.Transit
             }
         }
         #endregion Path & 路径
-
-        /// <summary>
-        /// 软件版本号·格式：v 0.0
-        /// </summary>
+        //软件版本号·格式：v 0.0
         public static string GetVer
         {
             get
@@ -79,9 +66,7 @@ namespace PFM.DawnXZ.Library.Transit
                 return string.Format("v {0}", GetVersion);
             }
         }
-        /// <summary>
-        /// 软件版本号·格式：0.0
-        /// </summary>
+        //软件版本号·格式：0.0
         public static string GetVersion
         {
             get
@@ -90,26 +75,19 @@ namespace PFM.DawnXZ.Library.Transit
             }
         }
         #endregion Attribute & 属性
-
         #region Constructed function & 构造函数
-        /// <summary>
-        /// 构造函数
-        /// </summary>
+        //构造函数
         public Tools()
         { }
         #endregion Constructed function & 构造函数
-
         #region Member method & 成员方法
-
         #region public method & 公共方法
-        /// <summary>
         /// <函数：Encode>
         /// 作用：将字符串内容转化为16进制数据编码，其逆过程是Decode
         /// 参数说明：
         /// strEncode 需要转化的原始字符串
         /// 转换的过程是直接把字符转换成Unicode字符,比如数字"3"-->0033,汉字"我"-->U+6211
         /// 函数decode的过程是encode的逆过程.
-        /// </summary>
         /// <param name="strEncode"></param>
         /// <returns></returns>
         public static string EncodeHex(string strEncode)
@@ -121,10 +99,8 @@ namespace PFM.DawnXZ.Library.Transit
             }
             return strReturn;
         }
-        /// <summary>
         /// <函数：Decode>
         /// 作用：将16进制数据编码转化为字符串，是Encode的逆过程
-        /// </summary>
         /// <param name="strDecode"></param>
         /// <returns></returns>
         public static string DecodeHex(string strDecode)
@@ -137,11 +113,9 @@ namespace PFM.DawnXZ.Library.Transit
             return sResult;
         }
         #endregion public method & 公共方法
-
         #region private method & 私有方法
 
         #endregion private method & 私有方法
-
         #endregion Member method & 成员方法
     }
 }
