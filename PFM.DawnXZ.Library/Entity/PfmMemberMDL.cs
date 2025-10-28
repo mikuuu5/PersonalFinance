d95cd2a4746eedc8a72b7f2bd52400f9ebcd710d
kkchina <143343474+kkchina@users.noreply.github.com>
2025-10-28 15:19:15 +0800

    Update PfmMemberMDL.cs

--- PFM.DawnXZ.Library/Entity/PfmMemberMDL.cs
+++ PFM.DawnXZ.Library/Entity/PfmMemberMDL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace PFM.DawnXZ.Library.Entity
@@ -9,15 +9,20 @@ namespace PFM.DawnXZ.Library.Entity
     [Serializable]
     public class PfmMemberMDL : EntityBase
     {
-
         #region 构造函数
-
         /// <summary>
         /// 构造函数
         /// </summary>
         public PfmMemberMDL()
         { }
+        #endregion
 
+        #region 私有字段
+        private long _mbrId;
+        private DateTime _mbrAddTime;
+        private string _mbrName;
+        private string _mbrRelation;
+        private string _mbrDescription;
         #endregion
 
         #region 公共属性
@@ -25,85 +30,48 @@ namespace PFM.DawnXZ.Library.Entity
         /// <summary>
         /// 系统编号
         /// </summary>
-        private long _mbrId;
-        /// <summary>
-        /// 系统编号
-        /// </summary>
         public long MbrId
         {
-            get { return this._mbrId; }
-            set
-            {
-                this._mbrId = value;
-                RaisePropertyChanged("MbrId");
-            }
+            get { return _mbrId; }
+            set { _mbrId = value; RaisePropertyChanged("MbrId"); }
         }
-        /// <summary>
-        /// 添加时间
-        /// </summary>
-        private DateTime _mbrAddTime;
+
         /// <summary>
         /// 添加时间
         /// </summary>
         public DateTime MbrAddTime
         {
-            get { return this._mbrAddTime; }
-            set
-            {
-                this._mbrAddTime = value;
-                RaisePropertyChanged("MbrAddTime");
-            }
+            get { return _mbrAddTime; }
+            set { _mbrAddTime = value; RaisePropertyChanged("MbrAddTime"); }
         }
-        /// <summary>
-        /// 成员名称
-        /// </summary>
-        private string _mbrName;
+
         /// <summary>
         /// 成员名称
         /// </summary>
         public string MbrName
         {
-            get { return this._mbrName; }
-            set
-            {
-                this._mbrName = value;
-                RaisePropertyChanged("MbrName");
-            }
+            get { return _mbrName; }
+            set { _mbrName = value; RaisePropertyChanged("MbrName"); }
         }
-        /// <summary>
-        /// 成员关系
-        /// </summary>
-        private string _mbrRelation;
+
         /// <summary>
         /// 成员关系
         /// </summary>
         public string MbrRelation
         {
-            get { return this._mbrRelation; }
-            set
-            {
-                this._mbrRelation = value;
-                RaisePropertyChanged("MbrRelation");
-            }
+            get { return _mbrRelation; }
+            set { _mbrRelation = value; RaisePropertyChanged("MbrRelation"); }
         }
-        /// <summary>
-        /// 成员描述
-        /// </summary>
-        private string _mbrDescription;
+
         /// <summary>
         /// 成员描述
         /// </summary>
         public string MbrDescription
         {
-            get { return this._mbrDescription; }
-            set
-            {
-                this._mbrDescription = value;
-                RaisePropertyChanged("MbrDescription");
-            }
+            get { return _mbrDescription; }
+            set { _mbrDescription = value; RaisePropertyChanged("MbrDescription"); }
         }
 
         #endregion
-
     }
 }
