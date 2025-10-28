99da241d111fdfcc0e39c07b57c86261d9b14c64
kkchina <143343474+kkchina@users.noreply.github.com>
2025-10-28 15:15:09 +0800

    Update PfmAccountsMDL.cs

--- PFM.DawnXZ.Library/Entity/PfmAccountsMDL.cs
+++ PFM.DawnXZ.Library/Entity/PfmAccountsMDL.cs
@@ -1,23 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace PFM.DawnXZ.Library.Entity
 {
     /// <summary>
-    /// 账务账目
+    /// 账务账目实体类
     /// </summary>
     [Serializable]
     public class PfmAccountsMDL : EntityBase
     {
-
         #region 构造函数
-
         /// <summary>
         /// 构造函数
         /// </summary>
         public PfmAccountsMDL()
         { }
+        #endregion
 
+        #region 私有字段
+        private long _accId;
+        private int _mbrId;
+        private DateTime _accAddTime;
+        private byte _accStatus;
+        private string _accName;
+        private string _accCode;
+        private string _accCard;
+        private string _accPurpose;
+        private string _accDescription;
         #endregion
 
         #region 公共属性
@@ -25,149 +34,157 @@ namespace PFM.DawnXZ.Library.Entity
         /// <summary>
         /// 系统编号
         /// </summary>
-        private long _accId;
-        /// <summary>
-        /// 系统编号
-        /// </summary>
         public long AccId
         {
-            get { return this._accId; }
+            get => _accId;
             set
             {
-                this._accId = value;
-                RaisePropertyChanged("AccId");
+                if (_accId != value)
+                {
+                    _accId = value;
+                    RaisePropertyChanged(nameof(AccId));
+                }
             }
         }
+
         /// <summary>
-        /// 添加时间
-        /// </summary>
-        private int _mbrId;
-        /// <summary>
-        /// 添加时间
+        /// 会员编号
         /// </summary>
         public int MbrId
         {
-            get { return this._mbrId; }
+            get => _mbrId;
             set
             {
-                this._mbrId = value;
-                RaisePropertyChanged("MbrId");
+                if (_mbrId != value)
+                {
+                    _mbrId = value;
+                    RaisePropertyChanged(nameof(MbrId));
+                }
             }
         }
-        /// <summary>
-        /// 添加时间
-        /// </summary>
-        private DateTime _accAddTime;
+
         /// <summary>
         /// 添加时间
         /// </summary>
         public DateTime AccAddTime
         {
-            get { return this._accAddTime; }
+            get => _accAddTime;
             set
             {
-                this._accAddTime = value;
-                RaisePropertyChanged("AccAddTime");
+                if (_accAddTime != value)
+                {
+                    _accAddTime = value;
+                    RaisePropertyChanged(nameof(AccAddTime));
+                }
             }
         }
-        /// <summary>
-        /// 账目状态
-        /// </summary>
-        private byte _accStatus;
+
         /// <summary>
         /// 账目状态
         /// </summary>
         public byte AccStatus
         {
-            get { return this._accStatus; }
+            get => _accStatus;
             set
             {
-                this._accStatus = value;
-                RaisePropertyChanged("AccStatus");
+                if (_accStatus != value)
+                {
+                    _accStatus = value;
+                    RaisePropertyChanged(nameof(AccStatus));
+                }
             }
         }
-        /// <summary>
-        /// 账目名称
-        /// </summary>
-        private string _accName;
+
         /// <summary>
         /// 账目名称
         /// </summary>
         public string AccName
         {
-            get { return this._accName; }
+            get => _accName;
             set
             {
-                this._accName = value;
-                RaisePropertyChanged("AccName");
+                if (_accName != value)
+                {
+                    _accName = value;
+                    RaisePropertyChanged(nameof(AccName));
+                }
             }
         }
-        /// <summary>
-        /// 账目编码
-        /// </summary>
-        private string _accCode;
+
         /// <summary>
         /// 账目编码
         /// </summary>
         public string AccCode
         {
-            get { return this._accCode; }
+            get => _accCode;
             set
             {
-                this._accCode = value;
-                RaisePropertyChanged("AccCode");
+                if (_accCode != value)
+                {
+                    _accCode = value;
+                    RaisePropertyChanged(nameof(AccCode));
+                }
             }
         }
-        /// <summary>
-        /// 账目卡号
-        /// </summary>
-        private string _accCard;
+
         /// <summary>
         /// 账目卡号
         /// </summary>
         public string AccCard
         {
-            get { return this._accCard; }
+            get => _accCard;
             set
             {
-                this._accCard = value;
-                RaisePropertyChanged("AccCard");
+                if (_accCard != value)
+                {
+                    _accCard = value;
+                    RaisePropertyChanged(nameof(AccCard));
+                }
             }
         }
-        /// <summary>
-        /// 账目用途
-        /// </summary>
-        private string _accPurpose;
+
         /// <summary>
         /// 账目用途
         /// </summary>
         public string AccPurpose
         {
-            get { return this._accPurpose; }
+            get => _accPurpose;
             set
             {
-                this._accPurpose = value;
-                RaisePropertyChanged("AccPurpose");
+                if (_accPurpose != value)
+                {
+                    _accPurpose = value;
+                    RaisePropertyChanged(nameof(AccPurpose));
+                }
             }
         }
-        /// <summary>
-        /// 账目描述
-        /// </summary>
-        private string _accDescription;
+
         /// <summary>
         /// 账目描述
         /// </summary>
         public string AccDescription
         {
-            get { return this._accDescription; }
+            get => _accDescription;
             set
             {
-                this._accDescription = value;
-                RaisePropertyChanged("AccDescription");
+                if (_accDescription != value)
+                {
+                    _accDescription = value;
+                    RaisePropertyChanged(nameof(AccDescription));
+                }
             }
         }
 
         #endregion
 
+        #region 重写方法
+        /// <summary>
+        /// 重写ToString方法
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{AccName} ({AccCode})";
+        }
+        #endregion
     }
 }
